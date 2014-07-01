98e1b43a4d7c66c915c4ee3ba7d5343e55df0e09
Omwah <omwah@oyeh.org>
2014-06-30 19:41:08 -0700

    Add support for rotary switch, add delay to reduce cpu usage, set image resolution

--- camera.py
+++ camera.py
@@ -3,17 +3,25 @@
 import os
 import atexit
 from datetime import datetime
+import subprocess
 
 from wiringpi2 import *
 import picamera
 
 OUTPUT_DIR = '/home/pi/pictures'
+RESOLUTION = (2592, 1944)
 
 BUTTON_PIN = 7
 CHIRP_PIN = 6
 
+ROTARY_SWITCH = { 3:1, 2:2, 0:3, 4:4, 5:5 }
+
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
 g = GPIO()
 camera = picamera.PiCamera()
+camera.resolution = RESOLUTION
 
 g.pinMode(CHIRP_PIN, GPIO.OUTPUT)
 
@@ -37,9 +45,20 @@ def capture_still():
     camera.capture(os.path.join(OUTPUT_DIR, filename))
     return filename
 
+def rotary_action(new_pos):
+    print "Rotary pos:", new_pos
+
+    if new_pos == 5:
+        for _ in range(7):
+            chirp(20)
+            g.delay(200)
+        subprocess.call(["shutdown", "-h", "now"])
+
 if __name__ == '__main__':
     boot_ack()
     print '** Camera Ready **'
+
+    rotary_pos = 0
     while True:
         if g.digitalRead(BUTTON_PIN) == GPIO.LOW:
             chirp(5)
@@ -49,4 +68,11 @@ if __name__ == '__main__':
 
             while g.digitalRead(BUTTON_PIN) == GPIO.LOW:
                 g.delay(20)
+
+        for w_pin, r_pos in ROTARY_SWITCH.items():
+            if g.digitalRead(w_pin) == GPIO.LOW and r_pos != rotary_pos:
+                rotary_pos = r_pos
+                rotary_action(r_pos)
      
+        # Delay briefly so don't use 100% CPU
+        delay(200)
